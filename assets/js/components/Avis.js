d070ac4a828aa812121b624d0088bbd73bc5d6d8
Zakari Yassine <yassine.zakari.ext@orange.com>
2019-08-28 16:59:38 +0200

    ajout du formulaire envoi avis et finalisation de l'envoi

--- assets/js/components/Avis.js
+++ assets/js/components/Avis.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import FormAvis from '../components/FormAvis';
+import React, { Component, Fragment } from 'react'
+import FormAvis from '../components/FormAvis'
+import NavBar from '../components/NavBar'
+import PostedAvis from '../components/PostedAvis'
+import Footer from '../components/Footer'
 
 
 export default class Avis extends Component {
 
+    state = {
+        formSubmit: false
+    }
+
     handleSubmit = (data)=> {
         this.avisPost(data)
-}
+    }
 
     avisPost = (data) => {
         fetch('http://127.0.0.1:8000/api/avis/post', {
@@ -16,16 +23,31 @@ export default class Avis extends Component {
         'Content-Type': 'application/json',
         },
         body: JSON.stringify(data)
-        }).then(res => {
-        console.log(res);
-        }).catch(err => err);
+        })
+        .then(res => {
+        this.setState({
+            formSubmit: true
+        })
+        })
+        .catch(err => err);
     }
 
     render() {
-        return (
-            <div>
-                <FormAvis onSubmit={this.handleSubmit}></FormAvis>
-            </div>
-        );
+            return (
+                <Fragment>
+                    <NavBar/>
+                    <main className="avisMain">
+                        <section id="ancre">
+                            HÔTEL PARIMIS  ·  PARIS
+                            <p>
+                                <img className="avisparimis" src="http://localhost:8080/build/images/parimis_avis2.jpg" alt="PARIMIS avis"/>
+                            </p>
+                            <PostedAvis />
+                            {this.state.formSubmit? null : <FormAvis onSubmit={this.handleSubmit}></FormAvis> }
+                        </section>
+                    </main>
+                    <Footer />
+                </Fragment>
+            );
     }
 }
--- assets/js/components/FormAvis.js
+++ assets/js/components/FormAvis.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import Rating from 'react-rating'
+
 
 export default class FormAvis extends Component {
 
     state = {
         pseudo: "",
-        note: "",
+        note: 1,
         avisTitre: "",
         commentaire: ""
     }
 
     handleSubmit = (e)=> {
         e.preventDefault();
-            this.props.onSubmit(this.state);
+        this.props.onSubmit(this.state);
         
     }
 
@@ -36,48 +38,63 @@ export default class FormAvis extends Component {
             pseudo: e.target.value
         });
     }
+        handleRatingChangenote = (rating)  => {
+            this.setState({...this.state,  note: rating })
+            }
 
     render() {
         return (
-            <form name="blog_post" className="form-horizontal" onSubmit={this.handleSubmit}>
-            <div id="blog_post">
-            <div className="form-group">
-                    <label className="col-sm-2 control-label  " htmlFor="blog_post_body">Pseudo</label>
-                    <div className="col-sm-10">
-                        <input  type="text"
-                                id="blog_post_body"
-                                placeholder="Pseudo..."
-                                value={this.state.pseudo}
-                                onChange={this.handlePseudoChange}
-                                className="form-control"/>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-2 control-label  " htmlFor="blog_post_title">Note</label>
-                    <div className="col-sm-10">
-                        <input  type="text"
-                                id="blog_post_title"
-                                placeholder="Note..."
-                                value={this.state.rate}
-                                onChange={this.handleRateChange}
-                                className="form-control"/>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="col-sm-2 control-label  " htmlFor="blog_post_body" >Titre</label>
-                    <div className="col-sm-10">
-                        <input  type="text"
-                                id="blog_post_body"
-                                placeholder="Titre..."
-                                value={this.state.title}
-                                onChange={this.handleTitleChange}
-                                className="form-control"/>
-                    </div>
+        <div className="container">
+            <article 
+            className= "col-md-8 offset-2" 
+            id="formAvis" 
+            style={{boxShadow: "-1px 0px 15px 10px rgba(244, 117, 142,.58)", marginTop:"100px" }}
+            >
+                <form 
+                name="blog_post" 
+                className="form-horizontal" 
+                onSubmit={this.handleSubmit}
+                >
+                    <h2 style={{marginTop:'20px'}}>Postez votre avis </h2>
+                        <div id="blog_post">
+                            <div className="form-group">
+                                <label className="col-sm-2 control-label  " htmlFor="blog_post_body">Pseudo</label>
+                                <div className="col-sm-8 offset-2">
+                                    <input  type="text"
+                                    id="blog_post_body"
+                                    placeholder="Pseudo..."
+                                    value={this.state.pseudo}
+                                    onChange={this.handlePseudoChange}
+                                    className="form-control"/>
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label className="col-sm-2 control-label  " htmlFor="blog_post_body" >Note</label>
+                                <div className="col-sm-8 offset-2">
+                                    <Rating 
+                                    name='note'
+                                    initialRating={this.state.note}
+                                    onChange = {this.handleRatingChangenote}
+                                    emptySymbol={<i className="far fa-star" style={{fontSize: '30px'}}></i>}
+                                    fullSymbol={<i className="fas fa-star" style={{fontSize: '30px'}}></i>}
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label className="col-sm-2 control-label  " htmlFor="blog_post_body" >Titre</label>
+                                <div className="col-sm-8 offset-2">
+                                    <input  type="text"
+                                            id="blog_post_body"
+                                            placeholder="Titre..."
+                                            value={this.state.title}
+                                            onChange={this.handleTitleChange}
+                                            className="form-control"/>
+                                </div>
                 </div>
                 <div className="form-group">
                     <label className="col-sm-2 control-label  " htmlFor="blog_post_body" >Commentaire</label>
-                    <div className="col-sm-10">
-                        <input  type="text"
+                    <div className="col-sm-8 offset-2">
+                        <textarea  type="text"
                                 id="blog_post_body"
                                 placeholder="Commentaire..."
                                 value={this.state.body}
@@ -87,7 +104,7 @@ export default class FormAvis extends Component {
                 </div>
                 <div className="form-group">
                     <div className="col-sm-2"></div>
-                    <div className="col-sm-10">
+                    <div className="col-sm-12" style= {{paddingBottom: '20px'}}>
                         <button type="submit"
                                 id="blog_post_submit"
                                 className="btn-default btn">
@@ -97,6 +114,8 @@ export default class FormAvis extends Component {
                 </div>
             </div>
         </form>
+        </article>
+        </div>
         )
     }
 }
